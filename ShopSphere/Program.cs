d2c25c5ee0c4adf1514be14b90aa963043704ce5
Hamza-Rimawi <hamzayjor@gmail.com>
2026-06-04 12:29:49 +0300

    Update Program.cs

--- ShopSphere/Program.cs
+++ ShopSphere/Program.cs
@@ -131,8 +131,8 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 
             // The secret key used to validate the JWT signature.
             // This must be the same key used when generating the token.
-            IssuerSigningKey = new SymmetricSecurityKey(
-                Encoding.UTF8.GetBytes("THIS_IS_A_VERY_SECRET_KEY_123456"))
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey))
+
         };
     });
 
